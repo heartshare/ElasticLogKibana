11e97ae87c45babab2bb3c8a92a2c81a85741f88
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-12-14 14:33:15 +0800

    ELK日志框架搭建

--- src/main/java/com/bonaparte/Application.java
+++ src/main/java/com/bonaparte/Application.java
@@ -1,5 +1,7 @@
 package com.bonaparte;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +10,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Application {
+    private final static Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args){
+        log.info("aaaa");
+        log.info("bbbb");
+        log.debug("cccc");
+        log.error("dddd");
+        log.warn("eeee");
         SpringApplication.run(Application.class, args);
     }
 }
